f72387b77747d39db4ca317c51628afd1c287477
sandeep reddy ch <ch.sandeepreddy544@gmail.com>
2017-12-23 19:09:12 -0600

    CSharp Basics on Ternary Conditional Operators

---
+++ CSharpConditionalStatements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharpConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a today Temaparature");
+            int userInputTemparature = Convert.ToInt16(Console.ReadLine());
+            Weather myweatherObject = new Weather();
+            bool output = myweatherObject.CanITakeUmbrella(userInputTemparature);
+            string message = (output == true) ? "Please take the umbrella" : "Please donot take the umbrella");
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+    public class Weather
+    {
+        public bool CanITakeUmbrella(int temarature)
+        {
+            if (temarature > 40)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ CSharpReferenceTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharpReferenceTypes
+{
+    class Program
+    {
+        //heap -->virtual address space--OS--RAM
+        //stack-->virtual address space--OS--RAM
+        //System.Object
+        //int myNumber = 10;
+        //string myFirstName = "Sandeep";
+        static void Main(string[] args)
+        {
+            // myNumber.ToString();
+            MyClass myClassobject = new MyClass();
+            //calling method
+            myClassobject.Display();
+
+
+        }
+    }
+
+    public class MyClass : System.Object
+    {
+
+
+        //ASP+Static/NonStatic+Return Type+MethodName(datype parameter1)
+        public void Display()
+        {
+            string myName = "Sandeep";
+            int myNumber = 10;
+            Console.Write(myName + myNumber);
+        }
+    }
+}
