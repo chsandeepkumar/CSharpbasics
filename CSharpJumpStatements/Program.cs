279196ead34a390d2cf4dee5edee01feccae12b7
sandeep reddy ch <ch.sandeepreddy544@gmail.com>
2017-12-25 12:07:02 -0600

    Jumpstatments added into the repo

---
+++ CSharpJumpStatements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpJumpStatements
+{
+    class Program
+    {
+        // static int a = 0;
+        static void Main(string[] args)
+        {
+            //return key importance
+
+            Console.WriteLine("Enter any number");
+            int mynumber = GetInputFromConsole();
+            Console.WriteLine("User Entered input is " + mynumber);
+            Console.ReadLine();
+        }
+
+        static int GetInputFromConsole()
+        {
+            if (string.IsNullOrWhiteSpace(Console.ReadLine())) return 0;
+            return Convert.ToInt16(Console.ReadLine());
+        }
+    }
+}
