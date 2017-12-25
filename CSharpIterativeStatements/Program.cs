0f8073d839deaf8fc2bf2a22433cafb778a51853
sandeep reddy ch <ch.sandeepreddy544@gmail.com>
2017-12-25 11:30:30 -0600

    Iterative statements using ForLoop in C#

---
+++ CSharpIterativeStatements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharpIterativeStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Name of a customer");
+            string input=Console.ReadLine();
+
+            //Initialization
+            //condition
+            //increment or decrement
+
+            string[] customersdetails = new string[10];
+            customersdetails[0] = "Sandeep";
+            customersdetails[1] = "Sandeep";
+            customersdetails[2] = "Prathyusha";
+            customersdetails[3] = "Veenil";
+          //  int i = 0;
+            for (int i = 0; i < customersdetails.Length-1; i++)          
+            {
+                
+                if (customersdetails[i]!=null && customersdetails[i].Contains(input))
+                {
+                    Console.WriteLine( customersdetails[i]+" name found");
+                }
+                else
+                {
+                    Console.WriteLine(input + " name Not found");
+
+                }
+             
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
