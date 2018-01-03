7da39f67baa4e3801569fd25f082f8d02c6bb3b7
sandeep reddy ch <ch.sandeepreddy544@gmail.com>
2018-01-02 20:38:18 -0600

    this is for polymorphsim method overloading

---
+++ CSharpPolymorphism/CalculatorOperations.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpPolymorphism
+{
+    class CalculatorOperations
+    {
+        public int Addition(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public float Addition(float firstNumber, float secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public decimal Addition(decimal firstNumber, decimal secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ CSharpPolymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorOperations myCalcopeationInstance = new CalculatorOperations();
+          var result=  myCalcopeationInstance.Addition(1,2);
+            Console.WriteLine("sum of two integer values"+result);
+
+            var result1=myCalcopeationInstance.Addition(1.0m, 1.0m);
+            Console.WriteLine("sum of two decimal values" + result1);
+
+            Console.ReadLine();
+        }
+    }
+}
