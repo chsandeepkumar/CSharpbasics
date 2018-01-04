50031277cca29c2223cf17e78009ed290e5ec1cb
sandeep reddy ch <ch.sandeepreddy544@gmail.com>
2018-01-03 20:50:12 -0600

    ADDED demo class changes

---
+++ CSharpClassDemo/Vineel.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpClassDemo
+{
+    //Created a class describe a person
+    public class Vineel
+    {
+        //Methods+Properties+Fileds+Variables
+        public string MyPhoneNumber { get; set; }//Property
+        int mynumber = 10;//variable
+        string SerachingForAjob()
+        {
+            return "Got Job"+MyPhoneNumber;
+        }
+    }
+   
+}
