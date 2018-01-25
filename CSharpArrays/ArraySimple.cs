cd90eb45b170f5884661cfeffecd30ae9e56f76b
sandeep reddy ch <ch.sandeepreddy544@gmail.com>
2018-01-24 18:11:26 -0600

    updated the simple array

---
+++ CSharpArrays/ArraySimple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpArrays
+{
+    class ArraySimple
+    {
+        public static void DisplayArray()
+        {
+            int[] arrayname = new int[10];
+
+            for (int i = 0; i <= arrayname.Length - 1; i++)
+            {
+                arrayname[i] = 2;
+            }
+            Console.WriteLine(arrayname[0].ToString());
+            Console.ReadLine();
+        }
+    }
+}
--- CSharpArrays/Program.cs
+++ CSharpArrays/Program.cs
@@ -6,10 +6,11 @@ namespace CSharpArrays
     {
         public static void Main(string[] arguments)
         {
-            Console.WriteLine("Displaying Elements using Single Dimensional Array");
-            SingleDimensionalArrayDemo.DisplayArray();
-            Console.WriteLine("Displaying Elements using multi Dimensional Array");
-            MultiDimensionalArrayDemo.DisplayArray();
+            //Console.WriteLine("Displaying Elements using Single Dimensional Array");
+            //SingleDimensionalArrayDemo.DisplayArray();
+            //Console.WriteLine("Displaying Elements using multi Dimensional Array");
+            //MultiDimensionalArrayDemo.DisplayArray();
+            ArraySimple.DisplayArray();
             Console.ReadLine();
         }
     }
